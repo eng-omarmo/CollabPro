dd2e50d60cfa9f63d25c7c8fd12f9bf719c2fe14
eng-omarmoo <omarmoo683>
2024-04-21 11:18:26 +0300

    improve all team retrieving logic

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -1,18 +1,51 @@
 const mongoose = require("mongoose");
 const Team = require("../models/teamModel");
+const Project = require("../models/projectModel");
+const User = require("../models/userModel");
 const getTeam = async (req, res) => {
     try {
-        const teams = await Team.find();
+        const authOrgId = req.user.orgId;
+
+        // Check if the user is an admin
+        const isAdmin = req.user.is_admin;
+
+        // If user is admin, they can retrieve all teams
+        if (isAdmin) {
+            const teams = await Team.find({ orgId: authOrgId });
+            if (!teams || teams.length === 0) {
+                return res.status(404).json({ message: "No teams found for the organization" });
+            }
+            return res.status(200).json({ message: "Teams found", teams });
+        }
+
+        // If user is not admin, check if they are a project manager of any project
+        const projectManager = await ProjectManager.findOne({ userId: req.user._id });
+        if (!projectManager) {
+            return res.status(401).json({ message: "Unauthorized: Only project managers or admins can access team information" });
+        }
+
+        // Get the projects managed by the user
+        const projects = await Project.find({ projectManagerId: projectManager._id });
+
+        // Get teams associated with the projects managed by the user
+        const teams = await Team.find({ orgId: authOrgId, projectId: { $in: projects.map(project => project._id) } });
+
+        if (!teams || teams.length === 0) {
+            return res.status(404).json({ message: "No teams found for the projects managed by the user" });
+        }
+
         res.status(200).json({ message: "Teams found", teams });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 };
 
+
 const getTeamById = async (req, res) => {
     try {
         const team = await Team.findById(req.params.id);
         const authOrgId = req.user.orgId;
+        //also check if user is admin or project manager of the team
         if (authOrgId.toString() !== team.orgId.toString() && req.user.is_admin === true) {
             return res.status(401).json({ message: "Unauthorized to access this organization" });
         }

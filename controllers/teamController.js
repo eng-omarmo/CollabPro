22bbda7b6828b0b94912143291b22706afac5bf1
eng-omarmoo <omarmoo683>
2024-04-21 11:26:38 +0300

    improve all specific team  retrieving logic for teams

--- controllers/teamController.js
+++ controllers/teamController.js
@@ -43,12 +43,38 @@ const getTeam = async (req, res) => {
 
 const getTeamById = async (req, res) => {
     try {
-        const team = await Team.findById(req.params.id);
-        const authOrgId = req.user.orgId;
-        //also check if user is admin or project manager of the team
-        if (authOrgId.toString() !== team.orgId.toString() && req.user.is_admin === true) {
-            return res.status(401).json({ message: "Unauthorized to access this organization" });
+        const teamId = req.params.id
+        if (!mongoose.Types.ObjectId.isValid(teamId)) {
+            return res.status(400).json({ message: "Invalid ID" });
         }
+        const authOrgId = req.user.orgId;
+        // Check if the user is an admin
+        const getUserStatus = await User.findOne({ _id: req.user.id });
+        if (getUserStatus.is_admin === true) {
+            const team = await Team.findById(teamId);
+            if (!team) {
+                return res.status(404).json({ message: "Team not found" });
+            }
+            res.status(200).json({ message: "Team found", team });
+        }
+        // If user is not admin, check if they are a project manager of any project
+        const projectManager = await ProjectManager.findOne({ userId: req.user.id });
+        if (!projectManager) {
+            return res.status(401).json({ message: "Unauthorized: Only project managers or admins can access team information" });
+        }
+        // Get the projects managed by the user
+        const projects = await Project.find({ projectManagerId: projectManager._id });
+        // Get teams associated with the projects managed by the user
+        const teams = await Team.find({ orgId: authOrgId, projectId: { $in: projects.map(project => project._id) } });
+        if (!teams || teams.length === 0) {
+            return res.status(404).json({ message: "No teams found for the projects managed by the user" });
+        }
+
+        const team = await Team.findById(teamId);
+        if (!team) {
+            return res.status(404).json({ message: "Team not found" });
+        }
+
         res.status(200).json({ message: "Team found", team });
     } catch (error) {
         res.status(500).json({ message: error.message });
